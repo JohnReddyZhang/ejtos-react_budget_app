9292afe092784417061491c3c6e1f259244b3f8a
John Zhang <zhzhang135@gmail.com>
2024-03-13 01:02:22 -0700

    fix warning

--- src/components/CurrencySelection.js
+++ src/components/CurrencySelection.js
@@ -1,8 +1,8 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const CurrencySelection = () => {
     const { availableCurrencies, currency, dispatch } = useContext(AppContext);
-    
+
     const handleClick = (symbol) => {
         dispatch({
             type: 'CHG_CURRENCY',
